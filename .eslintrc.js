9899ca71f5e5d88fd39b8efa157938670b994955
ludogwada <sinquet.ludovic@gmail.com>
2022-01-11 12:09:16 -0400

    eslint standart

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,9 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "standard"
+    ],
     "parserOptions": {
         "ecmaVersion": 13,
         "sourceType": "module"
